df083d2fce94e26e6b1432d256febe8f99fffbf6
Olga Maciaszek-Sharma <olga.maciaszek-sharma@broadcom.com>
2025-02-25 13:14:39 +0100

    Init.

---
+++ http-verification-client/src/main/java/com/example/http/verification/client/HttpVerificationClientApplication.java
@@ -0,0 +1,20 @@
+package com.example.http.verification.client;
+
+import com.example.http.verification.client.clients.PersonService;
+import com.example.http.verification.client.clients.VerificationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.interfaceclients.InterfaceClientGroup;
+
+@SpringBootApplication
+@InterfaceClientGroup(value = "verificationClient", baseUrl = "http://localhost:8081", httpServiceTypes = {VerificationService.class,
+		PersonService.class})
+public class HttpVerificationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpVerificationClientApplication.class, args);
+	}
+
+}
+
