f1f3621cb181c3bc4c15d8e85e27d5d787a9763b
Olga Maciaszek-Sharma <olga.maciaszek-sharma@broadcom.com>
2025-02-27 13:18:03 +0100

    Add sample code.

--- http-verification-client/src/main/java/com/example/http/verification/client/HttpVerificationClientApplication.java
+++ http-verification-client/src/main/java/com/example/http/verification/client/HttpVerificationClientApplication.java
@@ -1,15 +1,11 @@
 package com.example.http.verification.client;
 
-import com.example.http.verification.client.clients.PersonService;
-import com.example.http.verification.client.clients.VerificationService;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.interfaceclients.http.InterfaceClientGroup;
 
 @SpringBootApplication
-@InterfaceClientGroup(value = "verificationClient", httpServiceTypes = {VerificationService.class,
-		PersonService.class})
+@InterfaceClientGroup(value = "verificationClient")
 public class HttpVerificationClientApplication {
 
 	public static void main(String[] args) {
