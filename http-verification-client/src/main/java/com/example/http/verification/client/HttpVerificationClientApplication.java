a3db0214bac2b1861f486bcc89eb7e8df4008eb0
Olga Maciaszek-Sharma <olga.maciaszek-sharma@broadcom.com>
2025-02-27 13:47:02 +0100

    Add sample code.

--- http-verification-client/src/main/java/com/example/http/verification/client/HttpVerificationClientApplication.java
+++ http-verification-client/src/main/java/com/example/http/verification/client/HttpVerificationClientApplication.java
@@ -1,11 +1,13 @@
 package com.example.http.verification.client;
 
+import com.example.http.verification.client.clients.VerificationService;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.interfaceclients.http.InterfaceClientGroup;
 
 @SpringBootApplication
-@InterfaceClientGroup
+@InterfaceClientGroup(value = "verificationClient", httpServiceTypes = {VerificationService.class})
 public class HttpVerificationClientApplication {
 
 	public static void main(String[] args) {
---
+++ http-verification-client/src/main/java/com/example/http/verification/client/config/UserInterfaceClientsConfig.java
@@ -0,0 +1,26 @@
+package com.example.http.verification.client.config;
+
+import com.example.http.verification.client.clients.PersonService;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientHttpServiceProxyRegistry;
+
+/**
+ * @author Olga Maciaszek-Sharma
+ */
+@Configuration
+public class UserInterfaceClientsConfig {
+
+	@Bean
+	public RestClientHttpServiceProxyRegistry restClientHttpServiceProxyRegistry(RestClient.Builder restClientBuilder) {
+		RestClientHttpServiceProxyRegistry proxyRegistry = RestClientHttpServiceProxyRegistry.create(restClientBuilder);
+		proxyRegistry.registerGroup("programmaticVerificationClientGroup",
+				group -> {
+					group.addHttpServiceTypes(PersonService.class);
+					group.baseUrl("http://localhost:8081/");
+				});
+		return proxyRegistry;
+	}
+}
