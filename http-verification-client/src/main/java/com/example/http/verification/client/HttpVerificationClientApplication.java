7e981fc6c09b69844b470dce41629c04cb787919
Olga Maciaszek-Sharma <olga.maciaszek-sharma@broadcom.com>
2025-02-27 11:24:00 +0100

    Adjust to changes in FW and Boot.
    # Conflicts:
    #	http-verification-client/src/main/java/com/example/http/verification/client/HttpVerificationClientApplication.java

--- http-verification-client/src/main/java/com/example/http/verification/client/HttpVerificationClientApplication.java
+++ http-verification-client/src/main/java/com/example/http/verification/client/HttpVerificationClientApplication.java
@@ -5,10 +5,10 @@ import com.example.http.verification.client.clients.VerificationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.interfaceclients.InterfaceClientGroup;
+import org.springframework.boot.autoconfigure.interfaceclients.http.InterfaceClientGroup;
 
 @SpringBootApplication
-@InterfaceClientGroup(value = "verificationClient", baseUrl = "http://localhost:8081", basePackageClasses = VerificationService.class)
+@InterfaceClientGroup(value = "verificationClient", basePackageClasses = VerificationService.class)
 public class HttpVerificationClientApplication {
 
 	public static void main(String[] args) {
